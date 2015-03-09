1995c3ba66ab378710ff6007f8d2ae6d47fc04a8
Yuri Gorshenin <ygorshenin@chromium.org>
2015-03-10 00:34:44 +0300

    Added unittests to RMQ.

--- src/algo/rmq.h
+++ src/algo/rmq.h
@@ -8,7 +8,7 @@
 namespace algo {
 
 template <typename Value>
-using RMQTable = std::vector<std::vector<Value> >;
+using RMQIndex = std::vector<std::vector<Value> >;
 
 template <typename T>
 T FloorLog(const T& n) {
@@ -29,37 +29,37 @@ T CeilLog(const T& n) {
 }
 
 template <typename Value>
-void BuildRMQTable(size_t n, const Value* values, RMQTable<Value>& table) {
+void BuildRMQIndex(size_t n, const Value* values, RMQIndex<Value>& index) {
   if (!n) {
-    table.clear();
+    index.clear();
     return;
   }
-  table.resize(CeilLog(n));
-  table[0].assign(values, values + n);
+  index.resize(CeilLog(n));
+  index[0].assign(values, values + n);
   for (int p = 1; (static_cast<size_t>(1) << p) <= n; ++p) {
     size_t window_size = static_cast<size_t>(1) << p;
     size_t half_window_size = window_size >> 1;
 
-    table[p].resize(n - window_size + 1);
-    const std::vector<Value>& prev = table[p - 1];
-    std::vector<Value>& cur = table[p];
+    index[p].resize(n - window_size + 1);
+    const std::vector<Value>& prev = index[p - 1];
+    std::vector<Value>& cur = index[p];
     for (size_t i = 0; i + window_size <= n; ++i)
       cur[i] = std::min(prev[i], prev[i + half_window_size]);
   }
 }
 
 template <typename Value>
-Value RMQ(size_t from, size_t to, const RMQTable<Value>& table) {
+Value RMQ(size_t from, size_t to, const RMQIndex<Value>& index) {
   assert(to >= from);
   if (from == to)
     return std::numeric_limits<Value>::max();
   size_t size = to - from;
   size_t level = FloorLog(size);
   size_t window = static_cast<size_t>(1) << level;
-  assert(table.size() > level);
+  assert(index.size() > level);
   if (size == window)
-    return table[level][from];
-  return std::min(table[level][from], table[level][to - window]);
+    return index[level][from];
+  return std::min(index[level][from], index[level][to - window]);
 }
 
 }  // namespace algo
