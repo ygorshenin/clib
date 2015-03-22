0f77f363d417ccf298cbb1be1f2370eb6ef4a1c6
Yuri Gorshenin <y@mmaps.me>
2015-03-22 19:31:02 +0300

    Implemented matrix transposition algorithm.

---
+++ src/algo/numeric/matrix.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <cassert>
+#include <cstddef>
+
+#define CLS 64  // cache-line size
+
+namespace algo {
+
+namespace impl {
+
+template <typename T, template <typename> class Matrix>
+void TransposeImpl(size_t min_row,
+                   size_t max_row,
+                   size_t min_col,
+                   size_t max_col,
+                   const Matrix<T>& a,
+                   Matrix<T>& b);
+
+}  // namespace impl
+
+// Adapter from 1d array to 2d matrix.
+template <typename T>
+class MatrixAdapter {
+ public:
+  MatrixAdapter(size_t height, size_t width, T* buffer)
+      : height_(height), width_(width), buffer_(buffer) {}
+
+  inline size_t height() const { return height_; }
+
+  inline size_t width() const { return width_; }
+
+  inline size_t size() const { return height_ * width_; }
+
+  T& operator()(size_t row, size_t col) { return buffer_[row * width_ + col]; }
+
+  const T& operator()(size_t row, size_t col) const {
+    return buffer_[row * width_ + col];
+  }
+
+ private:
+  size_t height_;
+  size_t width_;
+  T* buffer_;
+};
+
+template <typename T, template <typename> class Matrix>
+void Transpose(const Matrix<T>& a, Matrix<T>& b) {
+  assert(a.height() == b.width());
+  assert(a.width() == b.height());
+
+  impl::TransposeImpl(0, a.height(), 0, a.width(), a, b);
+}
+
+namespace impl {
+
+template <typename T, template <typename> class Matrix>
+void TransposeImpl(size_t min_row,
+                   size_t max_row,
+                   size_t min_col,
+                   size_t max_col,
+                   const Matrix<T>& a,
+                   Matrix<T>& b) {
+  size_t height = max_row - min_row;
+  size_t width = max_col - min_col;
+
+  if (!height || !width)
+    return;
+
+  if (height * sizeof(T) <= CLS && width * sizeof(T) < CLS) {
+    for (size_t row = min_row; row < max_row; ++row) {
+      for (size_t col = min_col; col < max_col; ++col)
+        b(col, row) = a(row, col);
+    }
+    return;
+  }
+  size_t middle_row = min_row + height / 2;
+  size_t middle_col = min_col + width / 2;
+  TransposeImpl(min_row, middle_row, min_col, middle_col, a, b);
+  TransposeImpl(min_row, middle_row, middle_col, max_col, a, b);
+  TransposeImpl(middle_row, max_row, min_col, middle_col, a, b);
+  TransposeImpl(middle_row, max_row, middle_col, max_col, a, b);
+}
+
+}  // namespace impl
+
+}  // namespace algo
