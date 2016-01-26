45289ee56efe78cfdea93730bc3d11b1e89cbf9c
Yuri Gorshenin <mipt.vi00@gmail.com>
2016-01-26 23:14:11 +0300

    [graph] Added hacky Kuhn algorithm.

---
+++ src/algo/graph/kuhn.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <vector>
+
+namespace algo {
+template <typename Graph> class Kuhn {
+public:
+  Kuhn(const Graph &graph, int from, int to, std::vector<int> &match_to)
+      : graph_(graph), match_to_(match_to), from_(from), to_(to) {}
+
+  void Go() {
+    int n = graph_.NumVertices();
+    match_to_.assign(n, -1);
+    colors_.assign(n, -1);
+
+    std::vector<int> unmatched;
+    unmatched.reserve(to_ - from_);
+    GreedyMatch(unmatched);
+
+    for (int color = 0; !unmatched.empty(); ++color) {
+      bool ok = false;
+
+      int j = 0;
+      for (int i = 0; i < static_cast<int>(unmatched.size()); ++i) {
+        const int u = unmatched[i];
+        if (Match(u, color))
+          ok = true;
+        else
+          unmatched[j++] = u;
+      }
+      unmatched.resize(j);
+
+      if (!ok)
+        break;
+    }
+  }
+
+private:
+  void GreedyMatch(std::vector<int> &unmatched) {
+    for (int u = from_; u < to_; ++u) {
+      bool matched = false;
+      for (const auto &edge : graph_.OutEdges(u)) {
+        int v = edge.Target();
+        if (match_to_[v] < 0) {
+          match_to_[u] = v;
+          match_to_[v] = u;
+          matched = true;
+          break;
+        }
+      }
+      if (!matched)
+        unmatched.push_back(u);
+    }
+  }
+
+  bool Match(int u, int color) {
+    if (colors_[u] == color)
+      return false;
+    colors_[u] = color;
+    for (const auto &edge : graph_.OutEdges(u)) {
+      const int v = edge.Target();
+      const int w = match_to_[v];
+      if (w < 0 || Match(w, color)) {
+        match_to_[u] = v;
+        match_to_[v] = u;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const Graph &graph_;
+  std::vector<int> &match_to_;
+  std::vector<int> colors_;
+  int from_;
+  int to_;
+};
+} // namespace algo
---
+++ src/algo/graph/kuhn_unittest.cc
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "algo/graph/kuhn.h"
+
+namespace {
+struct Edge {
+  Edge(int to) : to_(to) {}
+
+  inline int Target() const { return to_; }
+
+  int to_;
+};
+
+template <typename Edge> struct Graph {
+  Graph(int n) : adj_(n) {}
+
+  inline int NumVertices() const { return adj_.size(); }
+  inline const std::vector<Edge> &OutEdges(int u) const { return adj_[u]; }
+
+  std::vector<std::vector<Edge>> adj_;
+};
+} // namespace
+
+namespace algo {
+TEST(Kuhn, Simple) {
+  // Test taken from http://www.spoj.com/problems/MATCHING/
+  const int ls = 5;
+  const int rs = 4;
+
+  std::vector<std::pair<int, int>> edges = {
+      {5, 2}, {1, 2}, {4, 3}, {3, 1}, {2, 2}, {4, 4}};
+
+  Graph<Edge> graph(ls + rs);
+  for (const auto &edge : edges)
+    graph.adj_[edge.first - 1].push_back(ls + edge.second - 1);
+
+  std::vector<int> match_to;
+  Kuhn<decltype(graph)> kuhn(graph, 0, ls, match_to);
+  kuhn.Go();
+
+  ASSERT_EQ(ls + rs, match_to.size());
+
+  int matched = 0;
+  for (int u = 0; u < static_cast<int>(match_to.size()); ++u) {
+    if (match_to[u] < 0)
+      continue;
+    if (u < ls)
+      ++matched;
+    int v = match_to[u];
+    if (u < ls) {
+      ASSERT_GE(v, ls);
+      ASSERT_LT(v, ls + rs);
+    } else {
+      ASSERT_GE(v, 0);
+      ASSERT_LT(v, ls);
+    }
+    ASSERT_EQ(u, match_to[v]);
+  }
+
+  ASSERT_EQ(3, matched);
+}
+}  // namespace algo
