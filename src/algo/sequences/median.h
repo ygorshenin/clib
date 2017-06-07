2337be90591feca167bac7437700796227a8fa09
Yuri Gorshenin <mipt.vi00@gmail.com>
2017-06-07 18:13:45 +0300

    [sequences] Implemented median search in two sorted arrays.

---
+++ src/algo/sequences/median.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+#include <vector>
+
+namespace algo {
+template <typename T>
+const T &GetKth(const T *a, size_t lena, const T *b, size_t lenb, size_t k) {
+  assert(std::is_sorted(a, a + lena));
+  assert(std::is_sorted(b, b + lenb));
+  assert(k < lena + lenb);
+
+  if (lena == 0)
+    return b[k];
+  if (lenb == 0)
+    return a[k];
+
+  const auto n = lena / 2;
+  const auto m = lenb / 2;
+  const auto h = n + m + 1;
+
+  assert(a[n] != b[m]);
+
+  if (a[n] > b[m])
+    return GetKth(b, lenb, a, lena, k);
+  if (k < h)
+    return GetKth(a, lena, b, m, k);
+  return GetKth(a + n + 1, lena - n - 1, b, lenb, k - n - 1);
+}
+
+template <typename T>
+const T &GetKth(const std::vector<T> &a, const std::vector<T> &b, size_t k) {
+  return GetKth(a.data(), a.size(), b.data(), b.size(), k);
+}
+} // namespace algo
---
+++ src/algo/sequences/median_unittest.cc
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+
+#include "algo/sequences/median.h"
+
+#include <vector>
+
+using namespace algo;
+using namespace std;
+
+namespace {
+TEST(Median, Smoke) {
+  {
+    const vector<int> a = {100};
+    const vector<int> b = {};
+    ASSERT_EQ(100, GetKth(a, b, 0));
+    ASSERT_EQ(100, GetKth(b, a, 0));
+  }
+
+  {
+    // The merged sequence is: 1, 2, 3, 4, 5, 6, 7, 8.
+    const vector<int> a = {2, 4, 7};
+    const vector<int> b = {1, 3, 5, 6, 8};
+    for (int i = 0; i < 8; ++i) {
+      ASSERT_EQ(i + 1, GetKth(a, b, i));
+      ASSERT_EQ(i + 1, GetKth(b, a, i));
+    }
+  }
+}
+} // namespace
