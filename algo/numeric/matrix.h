7d849de203f91ac3a9298fa2771426213dc6e613
Yuri Gorshenin <mipt.vi002@gmail.com>
2019-08-17 00:03:59 +0300

    Refactored matrix module.

--- algo/numeric/matrix.h
+++ algo/numeric/matrix.h
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <vector>
 
 namespace algo {
 
@@ -11,23 +12,55 @@ template <typename T>
 class MatrixAdapter {
 public:
   MatrixAdapter(size_t height, size_t width, T* buffer)
-      : height_(height), width_(width), size_(height_ * width_), buffer_(buffer) {}
+      : m_height(height), m_width(width), m_size(m_height * m_width), m_buffer(buffer) {}
 
-  inline size_t height() const { return height_; }
+  size_t Height() const { return m_height; }
+  size_t Width() const { return m_width; }
+  size_t Size() const { return m_size; }
 
-  inline size_t width() const { return width_; }
+  T& operator()(size_t row, size_t col) {
+    assert(row < m_height);
+    assert(col < m_width);
+    return m_buffer[row * m_width + col];
+  }
 
-  inline size_t size() const { return size_; }
-
-  T& operator()(size_t row, size_t col) { return buffer_[row * width_ + col]; }
-
-  const T& operator()(size_t row, size_t col) const { return buffer_[row * width_ + col]; }
+  const T& operator()(size_t row, size_t col) const {
+    assert(row < m_height);
+    assert(col < m_width);
+    return m_buffer[row * m_width + col];
+  }
 
 private:
-  const size_t height_;
-  const size_t width_;
-  const size_t size_;
-  T* const buffer_;
+  const size_t m_height;
+  const size_t m_width;
+  const size_t m_size;
+  T* const m_buffer;
+};
+
+template <typename T>
+class Matrix {
+public:
+  Matrix(size_t height, size_t width) : m_buffer(m_height * m_width), m_height(height), m_width(width) {}
+
+  size_t Height() const { return m_height; }
+  size_t Width() const { return m_width; }
+
+  T& operator()(size_t row, size_t col) {
+    assert(row < m_height);
+    assert(col < m_width);
+    return m_buffer[row * m_width + col];
+  }
+
+  const T& operator()(size_t row, size_t col) const {
+    assert(row < m_height);
+    assert(col < m_width);
+    return m_buffer[row * m_width + col];
+  }
+
+private:
+  std::vector<T> m_buffer;
+  const size_t m_height;
+  const size_t m_width;
 };
 
 template <typename T, template <typename> class Matrix>
@@ -46,41 +79,41 @@ class MatrixTranspose {
 public:
   static const int kCLS = 64;  // cache-line size estimation
 
-  MatrixTranspose(const Matrix<T>& a, Matrix<T>& b) : a_(a), b_(b) {
-    assert(a_.height() == b_.width());
-    assert(a_.width() == b_.height());
+  MatrixTranspose(const Matrix<T>& a, Matrix<T>& b) : m_a(a), m_b(b) {
+    assert(m_a.Height() == m_b.Width());
+    assert(m_a.Width() == m_b.Height());
   }
 
-  void Go() const { Go(0, a_.height(), 0, a_.width()); }
+  void Go() const { Go(0, m_a.Height(), 0, m_a.Width()); }
 
 private:
-  void Go(size_t min_row, size_t max_row, size_t min_col, size_t max_col) const {
+  void Go(size_t minRow, size_t maxRow, size_t minCol, size_t maxCol) const {
     static const size_t kLimit = std::max(kCLS / sizeof(T), static_cast<size_t>(1));
 
-    const size_t height = max_row - min_row;
-    const size_t width = max_col - min_col;
+    const size_t height = maxRow - minRow;
+    const size_t width = maxCol - minCol;
 
     if (!height || !width)
       return;
 
     if (height <= kLimit && width <= kLimit) {
-      for (size_t row = min_row; row < max_row; ++row) {
-        for (size_t col = min_col; col < max_col; ++col)
-          b_(col, row) = a_(row, col);
+      for (size_t row = minRow; row < maxRow; ++row) {
+        for (size_t col = minCol; col < maxCol; ++col)
+          m_b(col, row) = m_a(row, col);
       }
       return;
     }
 
-    const size_t middle_row = min_row + height / 2;
-    const size_t middle_col = min_col + width / 2;
-    Go(min_row, middle_row, min_col, middle_col);
-    Go(min_row, middle_row, middle_col, max_col);
-    Go(middle_row, max_row, min_col, middle_col);
-    Go(middle_row, max_row, middle_col, max_col);
+    const size_t middleRow = minRow + height / 2;
+    const size_t middleCol = minCol + width / 2;
+    Go(minRow, middleRow, minCol, middleCol);
+    Go(minRow, middleRow, middleCol, maxCol);
+    Go(middleRow, maxRow, minCol, middleCol);
+    Go(middleRow, maxRow, middleCol, maxCol);
   }
 
-  const Matrix<T>& a_;
-  Matrix<T>& b_;
+  const Matrix<T>& m_a;
+  Matrix<T>& m_b;
 };
 
 }  // namespace algo
