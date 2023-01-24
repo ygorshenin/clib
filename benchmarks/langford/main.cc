12e2a9d11cbd6e82acb8d8936aba97a75e394f62
Yury Gorshenin <mipt.vi002@gmail.com>
2023-01-24 12:56:05 +0400

    Added gflags deps.

--- benchmarks/langford/main.cc
+++ benchmarks/langford/main.cc
@@ -1,16 +1,31 @@
 #include <solvers/langford.h>
 
+#include <gflags/gflags.h>
+
 #include <iostream>
+#include <sstream>
 
 using namespace algo::solvers;
 using namespace std;
 
-int main() {
+DEFINE_bool(use_naive_solver, false, "When true, naive solver will be used");
+
+int main(int argc, char* argv[]) {
+  {
+    ostringstream os;
+    os << "Usage: echo n | " << argv[0] << " [flags]";
+    gflags::SetUsageMessage(os.str());
+  }
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   int n;
   cin >> n;
 
   uint64_t count = 0;
-  Langford{}.Solve(n, [&count](const vector<int>& /* xs */) { ++count; });
+  if (FLAGS_use_naive_solver)
+    Langford{}.SolveNaive(n, [&count](const vector<int>& /* xs */) { ++count; });
+  else
+    Langford{}.Solve(n, [&count](const vector<int>& /* xs */) { ++count; });
   cout << count << endl;
   return 0;
 }
