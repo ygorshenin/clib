12b4bdba80547a15c64330f1838071c82da5a840
Yury Gorshenin <mipt.vi002@gmail.com>
2024-04-06 03:35:34 +0300

    Added bits dictionary.

--- algo/bits/bits_unittest.cc
+++ algo/bits/bits_unittest.cc
@@ -10,16 +10,24 @@ TEST(Bits, PopCount) {
   ASSERT_EQ(static_cast<uint8_t>(64), PopCount(0xFFFFFFFFFFFFFFFF));
 }
 
-TEST(Bits, CeilLog) {
-  ASSERT_EQ(static_cast<uint8_t>(0), CeilLog(0x0));
-  ASSERT_EQ(static_cast<uint8_t>(0), CeilLog(0x1));
-  ASSERT_EQ(static_cast<uint8_t>(1), CeilLog(0x2));
-  ASSERT_EQ(static_cast<uint8_t>(1), CeilLog(0x3));
-  ASSERT_EQ(static_cast<uint8_t>(2), CeilLog(0x4));
+TEST(Bits, LoPos) {
+  ASSERT_EQ(static_cast<uint8_t>(64), LoPos(0x0));
+  ASSERT_EQ(static_cast<uint8_t>(0), LoPos(0x1));
+  ASSERT_EQ(static_cast<uint8_t>(3), LoPos(0x8));
+  ASSERT_EQ(static_cast<uint8_t>(63), LoPos(0x8000000000000000));
+  ASSERT_EQ(static_cast<uint8_t>(10), LoPos(0x400));
+}
 
-  ASSERT_EQ(static_cast<uint8_t>(6), CeilLog(0x7f));
-  ASSERT_EQ(static_cast<uint8_t>(7), CeilLog(0x80));
-  ASSERT_EQ(static_cast<uint8_t>(31), CeilLog(0xFFFFFFFF));
-  ASSERT_EQ(static_cast<uint8_t>(63), CeilLog(0xFFFFFFFFFFFFFFFF));
+TEST(Bits, HiPos) {
+  ASSERT_EQ(static_cast<uint8_t>(0), HiPos(0x0));
+  ASSERT_EQ(static_cast<uint8_t>(0), HiPos(0x1));
+  ASSERT_EQ(static_cast<uint8_t>(1), HiPos(0x2));
+  ASSERT_EQ(static_cast<uint8_t>(1), HiPos(0x3));
+  ASSERT_EQ(static_cast<uint8_t>(2), HiPos(0x4));
+
+  ASSERT_EQ(static_cast<uint8_t>(6), HiPos(0x7f));
+  ASSERT_EQ(static_cast<uint8_t>(7), HiPos(0x80));
+  ASSERT_EQ(static_cast<uint8_t>(31), HiPos(0xFFFFFFFF));
+  ASSERT_EQ(static_cast<uint8_t>(63), HiPos(0xFFFFFFFFFFFFFFFF));
 }
 }  // namespace algo
